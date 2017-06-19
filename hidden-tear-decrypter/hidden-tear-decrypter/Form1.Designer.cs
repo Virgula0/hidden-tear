18ea2ebb4f823bd43d9e76362d9913a5b46d4fd9
Virgula0 <angelo.rosa1999@gmail.com>
2017-06-19 21:18:18 +0200

    Add files via upload

---
+++ hidden-tear-decrypter/hidden-tear-decrypter/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace hidden_tear_decrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 20);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(52, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(56, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(316, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Decrypt My Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label2.Location = new System.Drawing.Point(61, 239);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "hidden tear decrypter\r\ncoded by Uktu Sen(Jani) / uktusen.com\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(99, 153);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Files Decrypted!";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(141, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Processing...";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Visible = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(85, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(262, 36);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "No files decrypted!";
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(417, 312);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HY Decryptor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ hidden-tear-decrypter/hidden-tear-decrypter/Form1.cs
@@ -0,0 +1,225 @@
+﻿/*
+ _     _     _     _              _                  
+| |   (_)   | |   | |            | |                 
+| |__  _  __| | __| | ___ _ __   | |_ ___  __ _ _ __ 
+| '_ \| |/ _` |/ _` |/ _ \ '_ \  | __/ _ \/ _` | '__|
+| | | | | (_| | (_| |  __/ | | | | ||  __/ (_| | |   
+|_| |_|_|\__,_|\__,_|\___|_| |_|  \__\___|\__,_|_| 
+ * Coded by Utku Sen(Jani) / August 2015 Istanbul / utkusen.com 
+ * hidden tear may be used only for Educational Purposes. Do not use it as a ransomware!
+ * You could go to jail on obstruction of justice charges just for running hidden tear, even though you are innocent.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Security.AccessControl;
+
+
+namespace hidden_tear_decrypter
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 action, UInt32 uParam, String vParam, UInt32 winIni);
+        string userName = Environment.UserName;
+        string userDir = "C:\\";
+        string backgroundImageUrl = "http://imgur.com/a/Yu6q9"; //desktop background picture
+        int decrypted;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            // Set your salt here, change it to meet your flavor:
+            // The salt bytes must be at least 8 bytes.
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                     
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                        
+                  
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        public void DecryptFile(string file,string password)
+        {
+
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+
+            File.WriteAllBytes(file, bytesDecrypted);
+            string extension = System.IO.Path.GetExtension(file);
+            string result = file.Substring(0, file.Length - extension.Length);
+            System.IO.File.Move(file, result);
+
+        }
+
+        public void DecryptDirectory(string location)
+        {
+            try
+            {
+                label5.Visible = false;
+                label3.Visible = false;
+                string password = textBox1.Text;
+
+                string[] files = Directory.GetFiles(location);
+                string[] childDirectories = Directory.GetDirectories(location);
+                for (int i = 0; i < files.Length; i++)
+                {
+                    string extension = Path.GetExtension(files[i]);
+                    if (extension == ".locked")
+                    {
+                        DecryptFile(files[i], password);
+                        decrypted++;
+                    }
+                }
+                for (int i = 0; i < childDirectories.Length; i++)
+                {
+                    DecryptDirectory(childDirectories[i]);
+                }
+                if (decrypted > 0)
+                {
+                    label3.Visible = true;
+                }
+                else
+                {
+                    label5.Visible = true;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+        }
+
+        public void changeDesktop()
+        {
+            string backgroundImageName = userDir + userName + "\\ransom.jpg";
+            SetWallpaperFromWeb(backgroundImageUrl, backgroundImageName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path_1 = "Users\\";
+            label4.Visible = true;
+            DialogResult result = MessageBox.Show("Decrypting files... Please wait while doesn't appears Files decrypted and don't worry if software doesn't answear.", "Press ok to start decryption process.", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            if (result == DialogResult.Cancel)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                decrypted = 0;
+                string fullpath_1 = userDir + path_1 + userName + "\\Links";
+                string fullpath_2 = userDir + path_1 + userName + "\\Contacts";
+                string fullpath_3 = userDir + path_1 + userName + "\\Desktop";
+                string fullpath_4 = userDir + path_1 + userName + "\\Documents";
+                string fullpath_5 = userDir + path_1 + userName + "\\Downloads";
+                string fullpath_6 = userDir + path_1 + userName + "\\Pictures";
+                string fullpath_7 = userDir + path_1 + userName + "\\Music";
+                string fullpath_8 = userDir + path_1 + userName + "\\OneDrive";
+                string fullpath_9 = userDir + path_1 + userName + "\\Saved Games";
+                string fullpath_10 = userDir + path_1 + userName + "\\Favorites";
+                string fullpath_11 = userDir + path_1 + userName + "\\Searches";
+                string fullpath_12 = userDir + path_1 + userName + "\\Videos";
+                DecryptDirectory(fullpath_1);
+                DecryptDirectory(fullpath_2);
+                DecryptDirectory(fullpath_3);
+                DecryptDirectory(fullpath_4);
+                DecryptDirectory(fullpath_5);
+                DecryptDirectory(fullpath_6);
+                DecryptDirectory(fullpath_7);
+                DecryptDirectory(fullpath_8);
+                DecryptDirectory(fullpath_9);
+                DecryptDirectory(fullpath_10);
+                DecryptDirectory(fullpath_11);
+                DecryptDirectory(fullpath_12);
+
+                if (decrypted > 0)
+                {
+                    changeDesktop();
+                }
+
+                label4.Visible = false;
+
+            }
+
+        }
+
+        //Changes desktop background image
+        public void SetWallpaper(String path)
+        {
+            SystemParametersInfo(0x14, 0, path, 0x01 | 0x02);
+        }
+
+        //Downloads image from web
+        private void SetWallpaperFromWeb(string url, string path)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(new Uri(url), path);
+                SetWallpaper(path);
+            }
+            catch { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
